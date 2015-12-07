f4347aefab1ec37bae5639dc145a0aaf1c2ae738
aaron-hammond <aaron.hammond@live.com>
2015-12-07 10:26:38 +0000

    initial code commmit

---
+++ dnx.foo.tests/AddNumbersSpecs/AddSomeNumbersTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace dnx.foo.tests.AddNumbersSpecs
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class AddSomeNumbersTests : GivenA<AddSomeNumbers, int>
+    {
+        protected override void When()
+        {
+            base.When();
+
+            Result = Target.AddTwoNumbers(10, 10);
+        }
+
+        [Then]
+        public void TheResultIs20()
+        {
+            Assert.Equal(Result, 20);
+        }
+    }
+}
---
+++ src/dnx.foo/AddSomeNumbers.cs
@@ -0,0 +1,12 @@
+﻿namespace dnx.foo
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class AddSomeNumbers
+    {
+        public int AddTwoNumbers(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+    }
+}
